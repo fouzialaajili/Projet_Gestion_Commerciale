6d581e0017edff63a1e0b52ef248318b2549aee8
fouzialaajili <fouzialaajili@gmail.com>
2018-07-11 12:23:33 +0200

    Ajoutez des fichiers projet.

---
+++ Store.web/App_Start/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store.web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+           
+        }
+    }
+}
